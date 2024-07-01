c047d5203e314aecd7d7b9561936cebb62f9e192
Do Tran <dotran@Dos-MacBook-Pro.local>
2024-07-02 00:05:14 +0700

    add folder structure

---
+++ lib/features/splash/presentation/splash_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SplashScreen extends HookConsumerWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
